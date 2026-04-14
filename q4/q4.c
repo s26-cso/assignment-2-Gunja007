cd1f1c97519fb5b2a953bed42ace61776ae70a20
samhith <rao.samhith@gmail.com>
2026-04-14 14:49:30 +0530

    fixed q4.c

--- q4/q4.c
+++ q4/q4.c
@@ -7,12 +7,12 @@ int main() {
 
     while(scanf("%5s %d %d", op, &num1, &num2) == 3){
         char libname[20];
-        snprintf(libname, sizeof(libname), "./lib%s.so", op);
-        void *handle = dlopen(libname, RTLD_LAZY);
+        snprintf(libname, sizeof(libname), "./lib%s.so", op);//joins the strings and trims it safely to avoid buffer overflow
+        void *libptr = dlopen(libname, RTLD_LAZY);//loads the shared library, RTLD_LAZY means that the symbols are processed as needed
         typedef int (*op_func)(int, int);
-        op_func func = (op_func)dlsym(handle, op);
+        op_func func = (op_func)dlsym(libptr, op);//gets the function from the shared file
         printf("%d\n", func(num1, num2));
-        dlclose(handle);
+        dlclose(libptr);
     }
     return 0;
 }
