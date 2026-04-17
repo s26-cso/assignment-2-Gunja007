d7913701c5f59f85b661a597acc082664c82f09f
samhith <rao.samhith@gmail.com>
2026-04-17 10:36:21 +0530

    rewrite q5

--- q5/q5.s
+++ q5/q5.s
@@ -1,81 +1,87 @@
-.globl main
-
-.section .data
-filename: .string "input.txt"
-reading_mode: .string "r"
+.data
 yes: .string "Yes\n"
 no: .string "No\n"
+file: .string "input.txt"
+read: .string "r"
 
-.section .text
+.text
+.globl main
 main:
-    add sp, sp, -48 #create space on stack
-    sd ra, 40(sp)
-    sd s0, 32(sp)
-    sd s1, 24(sp)
-    sd s2, 16(sp)
-    sd s3, 8(sp)
-    sd s4, 0(sp) #saved return address and saved registers to be used on stack
+    # make space on the stack
+    addi sp, sp, -32
+    sd ra, 24(sp)
+    sd s0, 16(sp)       # s0 = file pointer
+    sd s1, 8(sp)        # s1 = file size    
 
-    la a0, filename
-    la a1, reading_mode #load address of filename to a0 and address of reading mode to a1 as arguments for fopen
+    # open input.txt
+    la a0, file         # pass the file path in ao
+    la a1, read         # pass reading mode in a1
     call fopen
-    mv s0, a0 #move filepointer to s0 to save it for future use
- 
-    li a1, 0 #argument for fseek, offset = 0
-    li a2, 2 #argument for fseek, 2 means seekend
-    call fseek #seeks end of file
+    mv s0, a0           # save file pointer in s0
 
-    mv a0, s0 #argument for ftell
-    call ftell #ftell gives us the current cursor position which happens to be at the end after fseek 
-    add s1, a0, -1 #we store the index of the last element in s1 (s1 becomes right ptr) by using result of ftell-1
-    li s2, 0 #left ptr
-
-loop:
-    bge s2, s1, printyes #if left ptr crosses or reaches right ptr then all elements have been checked and the input is a palindrome
-
-    mv a0, s0
-    mv a1, s2
-    li a2, 0
-    call fseek #fseek(fp, left, 0) since a2=0 fseek sets the cursor to left pos
-
-    mv a0, s0
-    call fgetc #gets character at cursor position i.e. left ptr
-    mv s3, a0 #move left character to s3
-
-    mv a0, s0
-    mv a1, s1
-    li a2, 0
-    call fseek #set cursor to right position
-
-    mv a0, s0
-    call fgetc #gets character at cursor poition i.e. right ptr
-    mv s4, a0 #store right character in s4
+    # getting total file size
+    li a1, 0        # a0 already contains the file pointer
+    li a2, 2        # we have pass 2 as the arg to seek end
+    call fseek
     
-    bne s3, s4, printno #if character at left ptr != character at right ptr then the input cannot be a palindrome
-    
-    add s2, s2, 1 #increment leftptr
-    add s1, s1, -1 #decrement rightptr
-    j loop
+    # ftell(file)
+    mv a0, s0
+    call ftell
+    mv s1, a0       # s1 = file size (n)
 
-printyes:
+    # We compare head (i) and tail (n-1-i)
+    li s2, 0        # left index
+    addi s3, s1, -1 # right index
+
+.loop:
+    # loop until the pointers meet or cross
+    bge s2, s3, .is_palindrome
+    
+    # get character at left index (s2)
+    mv a0, s0       # a0=file pointer
+    mv a1, s2       # a1=offset value
+    li a2, 0        # seek from the start
+    call fseek
+    mv a0, s0
+    call fgetc
+    mv t0, a0       # char 1 in t0
+    
+    # get character at right index (s3)
+    mv a0, s0
+    mv a1, s3
+    li a2, 0        
+    call fseek
+    mv a0, s0
+    call fgetc
+    mv t1, a0       # char 2 in t1
+    
+    # compare characters
+    bne t0, t1, .not_palindrome
+    
+    # move pointers
+    addi s2, s2, 1  # increment by 1
+    addi s3, s3, -1 # decrement by 1
+    j .loop
+
+.is_palindrome:
     la a0, yes
     call printf
-    j done
+    j .close
 
-printno:
+.not_palindrome:
     la a0, no
     call printf
+    j .close
 
-done:
-    ld ra, 40(sp)
-    ld s0, 32(sp)
-    ld s1, 24(sp)
-    ld s2, 16(sp)
-    ld s3, 8(sp)
-    ld s4, 0(sp)
-    add sp, sp, 48 #restore stack
+.close:
+    mv a0, s0
+    call fclose
+
+.exit:
+    # close the stack
+    li a0, 0
+    ld ra, 24(sp)
+    ld s0, 16(sp)
+    ld s1, 8(sp)
+    addi sp, sp, 32
     ret
-
-
-
-
